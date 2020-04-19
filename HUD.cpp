a15f575e7338997ca03a3bd5d8569bc551a86105
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 16:36:44 -0700

    create HUD defenition skeleton code

--- HUD.cpp
+++ HUD.cpp
@@ -1,1 +1,17 @@
 #include "HUD.h"
+
+HUD::HUD() {
+
+}
+
+void HUD::increaseScore(int amount) {
+}
+
+void HUD::decrementLives() {
+}
+
+void HUD::drawLivesSprites(sf::RenderWindow& window) {
+}
+
+void HUD::drawScore(sf::RenderWindow& window) {
+}
