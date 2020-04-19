ed46d6d38484dbbb21cd967eda2cdb9a4121da8d
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 16:55:56 -0700

    Implement drawLivesSprites in HUD

--- HUD.cpp
+++ HUD.cpp
@@ -1,4 +1,5 @@
 #include "HUD.h"
+#include <iostream>
 
 /*
 Name: HUD constructor
@@ -25,7 +26,37 @@ void HUD::decrementLives() {
 	lives--;
 }
 
+/*
+Name: drawLivesSprites
+Description: Draw the number of lives to the bottom of the screen and a player sprite
+	for each life minus 1 (to account for the current player).
+*/
 void HUD::drawLivesSprites(sf::RenderWindow& window) {
+	sf::Texture texture;
+	sf::Sprite sprite;
+	sf::Text livesText;
+	sf::Font font;
+
+	if (!font.loadFromFile("arial.ttf"))
+		std::cerr << "Error loading arial font in HUD" << std::endl;
+	// Draw lives as a number
+	livesText.setFont(font);
+	livesText.setString(std::to_string(lives));
+	livesText.setPosition(250, 760);
+	livesText.setCharacterSize(20);
+	livesText.setFillColor(sf::Color::White);
+
+	window.draw(livesText);
+
+	// Draw lives as sprites
+	if (!texture.loadFromFile("player.png"))
+		std::cerr << "Error in HUD::drawLivesSprites" << std::endl;
+
+	sprite.setTexture(texture);
+	for (int i = 0; i < lives - 1; i++) {
+		sprite.setPosition((i + 1) * 100 - 50, 750);
+		window.draw(sprite);
+	}
 }
 
 void HUD::drawScore(sf::RenderWindow& window) {
