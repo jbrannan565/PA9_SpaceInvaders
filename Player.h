0fd4570671c55d8408ce348768c09a5a88d35c3e
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 15:40:59 -0700

    Create Player constructor, add pTexture and pSprite to Player, create player.png as texture for the player

--- Player.h
+++ Player.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <string>
+#include <iostream>
+
 class Player
 {
-};
+private:
+	sf::Texture pTexture;
+	sf::Sprite pSprite;
 
+public:
+	Player() {
+		if (!pTexture.loadFromFile("player.png"))
+			std::cerr << "Error in Player constructor" << std::endl;
+		pSprite.setTexture(pTexture);
+		pSprite.setPosition(250-25, 700);
+	}
+};
