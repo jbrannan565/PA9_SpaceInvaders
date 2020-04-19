2acf96e2eabea8ccb4d9aacbdad359c9b8918199
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 15:51:52 -0700

    Create Bunker interface

--- Bunker.h
+++ Bunker.h
@@ -1,5 +1,19 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class Bunker
 {
+private:
+	sf::Texture pTexture;
+	sf::Sprite pSprite;
+
+public:
+	Bunker();
+
+	void drawBunker(sf::RenderWindow& window);
+
+	void takeHit();
+
+	const sf::Vector2f& getPosition();
 };
 
