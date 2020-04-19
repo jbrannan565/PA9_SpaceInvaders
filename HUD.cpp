0a684500f5cbb10af45e18e4328b3e851a639159
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 16:37:38 -0700

    Implement  HUD constructor

--- HUD.cpp
+++ HUD.cpp
@@ -1,7 +1,12 @@
 #include "HUD.h"
 
+/*
+Name: HUD constructor
+Description: Init lives to 3 and score to 0.
+*/
 HUD::HUD() {
-
+	lives = 3;
+	score = 0;
 }
 
 void HUD::increaseScore(int amount) {
