c7f05d2f077ad97adaea6e9d165d567cb2a1d9e3
Jared J Brannan <jbrannan565@gmail.com>
2020-04-19 16:35:19 -0700

    Create HUD interface

--- HUD.h
+++ HUD.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 class HUD
 {
+private:
+	int score;
+	int lives;
+
+public:
+	HUD();
+
+	void increaseScore(int amount);
+
+	void decrementLives();
+
+	void drawLivesSprites(sf::RenderWindow& window);
+
+	void drawScore(sf::RenderWindow& window);
+
 };
 
